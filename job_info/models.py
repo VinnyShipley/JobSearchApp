4e0415c2c715f829f0acc2b55e9e136a479bde39
Vinny <vinnyshipley@gmail.com>
2023-01-10 21:52:47 +0000

    job spreadsheet cell model created

--- job_info/models.py
+++ job_info/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+  company = models.CharField(max_length=256)
+  title = models.CharField(max_length=256)
+  location = models.CharField(max_length=50)
+  salary_range = models.CharField(max_length=20)
+  remote = models.BooleanField()
+  hybrid = models.BooleanField()
+  on_site = models.BooleanField()
+  application_date = models.DateField(auto_now=False, auto_now_add=False)
+  follow_up_date = models.DateField(auto_now=False, auto_now_add=False)
+  connections = models.CharField(max_length=10)
