5b66862fcc9794d09a5f2494dbbbed46e3dacfc4
Vinny <vinnyshipley@gmail.com>
2023-01-10 22:32:00 +0000

    CRUD is now possible baybeeeee

--- job_info/urls.py
+++ job_info/urls.py
@@ -3,7 +3,7 @@ from .views import JobCreateView, JobDeleteView, JobDetailView, JobListView
 
 urlpatterns = [
   path('', JobListView.as_view(), name='job_list'),
-  path('deatil/<int:pk>/', JobDetailView.as_view(), name='job_detail'),
+  path('detail/<int:pk>/', JobDetailView.as_view(), name='job_detail'),
   path('delete/<int:pk>/', JobDeleteView.as_view(), name='job_delete'),
   path('create/', JobCreateView.as_view(), name='job_create')
 ]
