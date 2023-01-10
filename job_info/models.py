2bc4ed29f8a5b9ad171c9b4e40f69f2d29c03de0
Vinny <vinnyshipley@gmail.com>
2023-01-10 22:11:34 +0000

    string method on model added and migrated

--- job_info/models.py
+++ job_info/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Job(models.Model):
+  user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
   company = models.CharField(max_length=256)
   title = models.CharField(max_length=256)
   location = models.CharField(max_length=50)
@@ -11,3 +13,6 @@ class Job(models.Model):
   application_date = models.DateField(auto_now=False, auto_now_add=False)
   follow_up_date = models.DateField(auto_now=False, auto_now_add=False)
   connections = models.CharField(max_length=10)
+  
+  def __str__(self):
+    return self.title
