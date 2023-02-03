3ee3b592ce8714204f61a2203dc5f955c9607c25
Vinny <vinnyshipley@gmail.com>
2023-02-03 02:38:39 +0000

    placeholders in pages, base url set

--- siiv/urls.py
+++ siiv/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('job/', include('job_info.urls'))
+    path('', include('job_info.urls'))
 ]
