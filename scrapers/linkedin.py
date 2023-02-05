87735c62ffd9c35d2c45947bf30ba17ce4ba67a5
Vinny <vinnyshipley@gmail.com>
2023-02-05 01:36:07 +0000

    Tired and hungry. back to mechanize, probably needs a selenium addition to function

--- scrapers/linkedin.py
+++ scrapers/linkedin.py
@@ -1,13 +1,7 @@
-from webbot import Browser
-driver = Browser()
-
-
-
 import mechanize
 
 browser = mechanize.Browser()
 browser.set_handle_robots(False)
-response = browser.open('http://www.linkedin.com')
-print(response.code)
-for f in browser.forms():
-  print(f)
+response = browser.open('http://www.google.com')
+browser.submit(id='gbqfbb')
+print(browser.code)
