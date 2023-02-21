e63d44926aff95c5ca0398807817a46894162401
Vinny <vinnyshipley@gmail.com>
2023-02-21 02:40:49 +0000

    nothing changed. working on fixing selenium import issue

--- scrapers/linkedin.py
+++ scrapers/linkedin.py
@@ -1,7 +1,6 @@
-import mechanize
+from selenium import webdriver
 
-browser = mechanize.Browser()
-browser.set_handle_robots(False)
-response = browser.open('http://www.google.com')
-browser.submit(id='gbqfbb')
-print(browser.code)
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+
+driver.close()
--- siiv/scraper.py
+++
@@ -1,6 +0,0 @@
-import mechanize
-
-browser = mechanize.Browser()
-browser.set_handle_robots(False)
-response = browser.open('http://www.google.com')
-print(response.code)
