25cab0993765d6c09f16fee8799df218fbc60fea
Vinny <vinnyshipley@gmail.com>
2023-02-28 03:27:47 +0000

    pushing for the calendar's sake. No real changes, trying to et the path and driver problem rectified

--- scrapers/linkedin.py
+++ scrapers/linkedin.py
@@ -1,9 +1,5 @@
+from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-driver = webdriver.Chrome(chrome_options = options, executable_path="C:\Program Files (x86)\chromedriver.exe")
-driver.get('http://google.com/')
-print("Chrome Browser Invoked")
-driver.quit()
+service = Service(executable_path="C:\Program Files (x86)\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
